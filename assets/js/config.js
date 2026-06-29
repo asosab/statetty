acf741e1b013bcb443446617e5ab40f1bf3e3d6f
Alejandro Sosa <asosab@gmail.com>
2026-06-29 11:08:14 -0400

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,4 @@
 var STATETTY_CONFIG = {
-  WS_API_BASE: "https://kodiak-mighty-hagfish.ngrok-free.app/api/statetty"
+  //WS_API_BASE: "https://kodiak-mighty-hagfish.ngrok-free.app/api/statetty"
+  WS_API_BASE: "http://161.97.176.137:3030/api/statetty"
 };
