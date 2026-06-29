f5db428a02401f1273424ec1cb104080a089f030
Alejandro Sosa <asosab@gmail.com>
2026-06-29 12:03:20 -0400

    tunnel con cloudflare

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,3 @@
 var STATETTY_CONFIG = {
-  //WS_API_BASE: "https://kodiak-mighty-hagfish.ngrok-free.app/api/statetty"
-  WS_API_BASE: "http://161.97.176.137:3030/api/statetty"
+  WS_API_BASE: "https://api.statetty.com/api/statetty"
 };
